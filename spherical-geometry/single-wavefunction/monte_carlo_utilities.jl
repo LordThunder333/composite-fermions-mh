6422ce759f6bd6fdb04f2e105e10b33f52907c83
LordThunder333 <62602885+LordThunder333@users.noreply.github.com>
2023-05-20 18:49:07 +0530

    Update monte_carlo_utilities.jl
    add reference for arm scale factor at some point.

--- spherical-geometry/single-wavefunction/monte_carlo_utilities.jl
+++ spherical-geometry/single-wavefunction/monte_carlo_utilities.jl
@@ -8,12 +8,6 @@ System Object.
 mutable struct system{T}
     state_vector::Vector{T} ### Current state.
     logpdf::Float64 ### Log of current state vector.
-    # amp_vector::Vector{ComplexF64} ### List of amplitudes.
-    # poly_main ### Polynomial system corresponding to the sampling distribution.
-    # var_list_main ### Variable list corresponding to the sampling distribution.
-    # poly_super ### Superset of polynomials
-    # var_list_super ### Superset of var_list
-    # index_list ### List of indices.
     params::Vector{Any}
 end
 
@@ -21,7 +15,6 @@ end
 Logpdf of the system with the vector of parameters passed in the same order as arguments.
 """
 function logpdf(ψ::system{T}) where {T}
-    # return logpdf(ψ.state_vector::Vector{T}, ψ.poly_main, ψ.var_list_main, ψ.poly_other,ψ.var_list_other,ψ.index_list,Tuple(ψ.params)...)
     return logpdf(ψ.state_vector::Vector{T}, Tuple(ψ.params)...)
 end
 
@@ -31,7 +24,6 @@ Samples object containing all metropolis-hastings samples, their logpdfs, accept
 mutable struct samples{T}
     sample_vector::Matrix{T}
     logpdf_vector::Vector{Float64}
-    # amp_vector::Matrix{ComplexF64}
     acceptance_ratio::Float64
     N_system::Int64 ### System size.
     N_samples::Int64 ### Number of samples.
@@ -56,15 +48,8 @@ function *(a::samples{T}, b::samples{T}) where T
     # println(T)
     N_system::Int64 = a.N_system
     N_samples::Int64 = a.N_samples + b.N_samples
-    # sample_vector = Array{T, 2}(undef,N_system,N_samples)
     sample_vector = hcat(a.sample_vector, b.sample_vector)
-    # logpdf_vector = Array{Float64, 1}(undef,N_samples)
     logpdf_vector = vcat(a.logpdf_vector,b.logpdf_vector)
-    # amp_vector = hcat(a.amp_vector,b.amp_vector)
-    # sample_vector[:,begin:a.N_samples] = copy(a.sample_vector)
-    # sample_vector[:,a.N_samples+1:end] = copy(b.sample_vector)
-    # logpdf_vector[begin:a.N_samples] = copy(a.logpdf_vector)
-    # logpdf_vector[a.N_samples+1:end] = copy(b.logpdf_vector)
     return samples(sample_vector, logpdf_vector,(a.N_samples * a.acceptance_ratio + b.N_samples * b.acceptance_ratio)/N_samples, N_system, N_samples)
 end
 """
@@ -93,7 +78,6 @@ function mh_batch_sampler!(batch_size::Int64, ψ::system{T}, q::Function, q_para
         n_accepted += Int64(mh_sampler!(ψ, q, q_params...))
         ans.sample_vector[:,i] = copy(ψ.state_vector)
         ans.logpdf_vector[i] = ψ.logpdf
-        # ans.amp_vector[:,i] = copy(ψ.amp_vector)
     end
     ans.acceptance_ratio::Float64 = n_accepted/batch_size
     return ans
@@ -107,7 +91,6 @@ function simulated_annealing_sampler!(ψ::system, temp::Float64, q::Function, q_
     logpdf_Y = logpdf(Y, Tuple(ψ.params)...)
     if logpdf_Y-ψ.logpdf > 0 || logpdf_Y-ψ.logpdf > temp*log(rand())
         ψ.state_vector = copy(Y)
-        # ψ.amp_vector = copy(amp_vector_Y)
         ψ.logpdf = logpdf_Y
         return true
     else
